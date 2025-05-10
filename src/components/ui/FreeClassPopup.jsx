1f6af1f243059ec78cb595b8c66a95a86f3d8347
NahinIntesher <nahinsikdar123@gmail.com>
2025-05-10 18:47:53 +0600

    a

--- src/components/ui/FreeClassPopup.jsx
+++ src/components/ui/FreeClassPopup.jsx
@@ -16,7 +16,7 @@ const FreeClassPopup = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsOpen(true);
-    }, 5000);
+    }, 4000);
 
     return () => clearTimeout(timer);
   }, []);
